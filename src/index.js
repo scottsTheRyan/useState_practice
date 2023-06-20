fd78c3cc66adf356701ca0990b16ba3dfcfcf876
thedarkness <sthomas25854@protonmail.com>
2023-06-20 10:25:04 -0700

    daily

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+function Fn1(){
+  console.log('scope');
+  let scope = 5;
+  console.log(scope);{
+    console.log('scope 2'){
+      let scope = 'different scope';
+        console.log(scope);
+    }
+    {
+      console.log('scope 3');
+        let scope = 'a third scope';
+          console.log(scope);
+    }
+  }
+}
+console.log Fn1();
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<fn1 />);
---
+++ src/practice_page.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+function Practice_page(){ 
+    return (
+         <div>
+            <h2>placeholder</h2>
+        </div>
+);
+};
+
+
+
+
+
+
+
+
+
+export default Practice_page;
