430b58ec8a4020aefd7493672e9c1cbb5d0d1847
thedarkness <sthomas25854@protonmail.com>
2023-06-20 12:47:07 -0700

    destructuring an object awe yeaaah

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
+
+// const image="https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/iss060e033385large.jpg"; 
+// const title="How to be a badass";
+// const author="OG G";
+
+
 const firstBook = {
   author: "total badass",
   title: "How to be a badass",
@@ -25,11 +31,13 @@ const BookList = () => {
 };
 
 const Book = (props) => {
+  console.log(props);
+  const {img, title, author} = props;
   return (
     <section>
-      <img src={props.img} alt="Total badass times two" />
-      <h2>{props.title}</h2>
-      <h1>{props.author}</h1>
+      <img src={img} alt="Total badass times two" />
+      <h2>{title}</h2>
+      <h1>{author}</h1>
     </section>
   );
 };
