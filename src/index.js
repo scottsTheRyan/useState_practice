af8d9b6974a6bf1394c178684837cd0c9c2a09da
thedarkness <sthomas25854@protonmail.com>
2023-06-20 10:46:36 -0700

    initial components

--- src/index.js
+++ src/index.js
@@ -1,26 +1,61 @@
-import React from "react";
+import React from "react"
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
+const BookList = () => {
+  return (
+    <section className = "book">
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
 
-function Fn1(){
-  console.log('scope');
-  let scope = 5;
-  console.log(scope);{
-    console.log('scope 2'){
-      let scope = 'different scope';
-        console.log(scope);
-    }
-    {
-      console.log('scope 3');
-        let scope = 'a third scope';
-          console.log(scope);
-    }
-  }
-}
-console.log Fn1();
+const Book = () => {
+  return (
+    <section>
+      <Image />
+      <Title />
+      <Author />
+    </section>
+  );
+};
 
 
+const Image = () => {
+  return (
+    <section className="books">
+      <img
+        src="https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/iss060e033385large.jpg"
+        alt="Nasa picture of the day"
+      />
+    </section>
+  );
+};
+
+const Author = () => {
+  return (
+    <section>
+      <h2>Will Peterson</h2>
+    </section>
+  );
+};
+
+const Title = () => {
+  return (
+    <section>
+      <h1>Why NASA rocks</h1>
+    </section>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<fn1 />);
+root.render(<BookList />);
+
+
+
+
+
+
+
