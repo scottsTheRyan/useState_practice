a4afa382930677f984a361e06e669211eb0c1d8e
thedarkness <sthomas25854@protonmail.com>
2023-06-20 11:33:10 -0700

    objects and dynamic props review

--- src/index.js
+++ src/index.js
@@ -3,18 +3,23 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-const img="https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/iss060e033385large.jpg";
-const title="Still a Badass";
-const author="Total Badass";
+const firstBook = {
+  author: "total badass",
+  title: "How to be a badass",
+  image: "https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/iss060e033385large.jpg",
+};
+
+const secondBook = {
+  author: "OG G",
+  title: "The badassery continues",
+  image: "https://www.nasa.gov/sites/default/files/thumbnails/image/curiosity_selfie.jpg",
+};
 
 const BookList = () => {
   return (
     <section className = "book">
-      <Book img={img} title={title} author={author}/>
-      <Book img={img} title={title} author={author}/>
-      <Book job="developer"/>
-      <Book job="Astronaut" number={23}/>
-      <Book jobNumber={24}/>
+      <Book img={firstBook.image} title={firstBook.title} author={firstBook.author}/>
+      <Book img={secondBook.image} title={secondBook.title} author={secondBook.author}/>
     </section>
   );
 };
@@ -22,12 +27,9 @@ const BookList = () => {
 const Book = (props) => {
   return (
     <section>
-      <img src={img} alt="Total badass times two" />
-      <h2>{title}</h2>
-      <h1>{author}</h1>
-      <h1>{props.job}</h1>
-      <h1>{props.number}</h1>
-      <h1>{props.jobNumber}</h1>
+      <img src={props.img} alt="Total badass times two" />
+      <h2>{props.title}</h2>
+      <h1>{props.author}</h1>
     </section>
   );
 };
