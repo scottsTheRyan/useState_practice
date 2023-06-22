86a82a6c6165574f95b90ab05e058c4a04278462
thedarkness <sthomas25854@protonmail.com>
2023-06-22 11:25:35 -0700

    again

--- src/index.js
+++ src/index.js
@@ -3,48 +3,40 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-
-
 const firstBook = {
-  author: "Jordan Moore",
-  title: "Interesting Facts For Curious Minds",
-  img: "./images/book-1.jpg",
+  author: 'Jordan Moore',
+  title: 'Interesting Facts For Curious Minds',
+  img: './images/book-1.jpg',
 };
 const secondBook = {
-  author: "James Clear",
-  title: "Atomic Habits",
-  img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
+  author: 'James Clear',
+  title: 'Atomic Habits',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
 };
 
 
-const Book = (props) => {
+function BookList() {
   return (
-    <section className="book">
-      <img src={props.img} alt="total badass" />
-      <h2>{props.title}</h2>
-      <h1>{props.author}</h1>
+    <section>
+      <Book author={firstBook.author} title={firstBook.title} img={firstBook.img}/>
+      <Book author={secondBook.author} title={secondBook.title} img={secondBook.img}/>
+
     </section>
   );
-};
+}
 
-
-const Booklist = () => {
+// have to add ClassName for styles to be applied,
+// className in react, class in html
+const Book = (props) => {
   return (
-    <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.author}
-        author={firstBook.title}
-      />
-      <Book
-        img={secondBook.img}
-        title={secondBook.author}
-        author={secondBook.title}
-      />
-      <Book />
-    </section>
+    <article className="book">
+      <img src={props.img} alt="Intresting facts from curious minds" />
+      <h2>{props.title}</h2>
+      <h1>{props.author}</h1>
+    </article>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Booklist />);
+
+root.render(<BookList />);
