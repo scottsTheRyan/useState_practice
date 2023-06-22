5f0f2f8fbade98d7489effcd6c694ab557e46172
thedarkness <sthomas25854@protonmail.com>
2023-06-22 16:31:52 -0700

    because destructuring is what plants like

--- src/index.js
+++ src/index.js
@@ -4,38 +4,47 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 const firstBook = {
-  author: 'Jordan Moore',
-  title: 'Interesting Facts For Curious Minds',
-  img: './images/book-1.jpg',
+  author: "Jordan Moore",
+  title: "Interesting Facts For Curious Minds",
+  img: "https://images-assets.nasa.gov/image/KSC-20230613-PH-KLS02_0073/KSC-20230613-PH-KLS02_0073~thumb.jpg",
 };
 const secondBook = {
-  author: 'James Clear',
-  title: 'Atomic Habits',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+  author: "James Clear",
+  title: "Atomic Habits",
+  img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
 };
-
-
-function BookList() {
-  return (
-    <section>
-      <Book author={firstBook.author} title={firstBook.title} img={firstBook.img}/>
-      <Book author={secondBook.author} title={secondBook.title} img={secondBook.img}/>
-
-    </section>
-  );
-}
 
 // have to add ClassName for styles to be applied,
 // className in react, class in html
 const Book = (props) => {
+  const { author, title, img } = props;
   return (
-    <article className="book">
-      <img src={props.img} alt="Intresting facts from curious minds" />
-      <h2>{props.title}</h2>
-      <h1>{props.author}</h1>
+      <article className="book">
+      <img src={img} alt="Intresting facts from curious minds" />
+      <h2>{title}</h2>
+      <h1>{author}</h1>
     </article>
   );
 };
+
+function BookList() {
+  return (
+    <section>
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
+    </section>
+  );
+}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
