d84f5526ca58e7d505d0e266b350b10ea462fc42
thedarkness <sthomas25854@protonmail.com>
2023-06-25 13:12:05 -0700

    just doing the reps

--- src/index.js
+++ src/index.js
@@ -3,70 +3,42 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-const books = [
-  {
-    author: "Jordan Moore",
-    title: "Interesting Facts For Curious Minds",
-    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
-    id: 1,
-  },
-  {
-    author: "James Clear",
-    title: "Atomic Habits",
-    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
-    id: 2,
-  },
-];
 
-function BookList() {
-  const getBook = (id) => {
-    const book = books.find((book) => book.id === id)
-    console.log(book);
-  }
+const books = [
+    {
+  author: "Bob White",
+  title: "Another total badass",
+  img: "https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/iss060e033385large.jpg",
+},
+{
+  author: "A Leon",
+  title: "Thie Planet",
+  img: "https://www.nasa.gov/sites/default/files/thumbnails/image/curiosity_selfie.jpg",
+},
+]
+
+
+const Book = (props) => {
+  const {img, title, author} = props;
   return (
-    <section className="booklist">
-      <EventExamples />
-      {books.map((dog) => {
-        const { img, title, author, id } = dog;
-        return <Book {...dog} key={dog.id} getBook={getBook}/>;
-      })}
+    <section className="book">
+      <img src={img} alt="total badass" />
+      <h2>{title}</h2> 
+      <h1>{author}</h1>
     </section>
   );
 };
 
 
-const EventExamples = () => {
-  const handleFormInput= (e) => {
-    console.log(e);
-    console.log(e.target.name);
-    console.log(e.target.value);
-    console.log('its a good show');
-  };
-  const handleButtonClick = () => {
-    alert('handle button click');
-  };
-
-  const handleFormSubmission = (e) => {
-    e.preventDefault();
-    console.log('form submitted');
-  };
-};
-
-const Book = (props) => {
-  const { img, title, author, getBook, id} = props;
-  const getSingleBook = () => {
-    getBook(id);
-  };
+const BookList = () => {
   return (
-    <article className="book">
-      <img src={img} alt="Intresting facts from curious minds" />
-      <h2>{title}</h2>
-      <button onClick={getSingleBook}>click me</button>
-      <h1>{author}</h1>
-    </article>
+    <section className="booklist">
+      {books.map((dog) => {
+        return <Book {...dog} key={dog.id} />
+      })};
+    </section>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(<BookList />);
