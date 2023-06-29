4570b14c1b47058e733f7307f953627509ac1833
thedarkness <sthomas25854@protonmail.com>
2023-06-29 13:37:48 -0700

    no commitment issues here

--- src/index.js
+++ src/index.js
@@ -5,24 +5,25 @@ import "./index.css";
 
 const books = [
   {
-    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
-    title: "Interesting Facts For Curious Minds",
     author: "Jordan Moore",
+    title: "Interesting Facts For Curious Minds",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
     id: 1,
   },
   {
-    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
-    title: "Atomic Habits",
     author: "James Clear",
+    title: "Atomic Habits",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
     id: 2,
   },
 ];
 
+
 const Book = (props) => {
-  const {img, title, author}=props;
-  return(
+  const {author, title, img} = props;
+  return (
     <section className="book">
-      <img src={img} alt="alt things"/>
+      <img src={img} alt="the things we think are alts"/>
       <h2>{title}</h2>
       <h1>{author}</h1>
     </section>
@@ -34,40 +35,39 @@ const BookList = () => {
     <section className="booklist">
       <EventExamples />
       {books.map((dog) => {
-        const { img, title, author, id } = dog;
+        const { author, title, img, id } = dog;
         return <Book {...dog} key={dog.id} />;
       })}
+      ;
     </section>
   );
-}
+};
 
 const EventExamples = () => {
   const handleFormSubmission = (e) => {
     e.preventdefault();
-  }
+    console.log(e);
+  };
+
   const handleFormInput = (e) => {
     console.log(e);
-    console.log('event happened')
-  }
+  };
+
   const handleButtonClick = () => {
-    alert('time to get this done!')
- }
+    alert('Clickin a button');
+  };
 
 
-return (
-  <section>
-    <form onSubmit={handleFormSubmission}>
-    <h2>Testing, is this form on?</h2>
-      <input onChange={handleFormInput}/>
-    </form>
-    <button onClick={handleButtonClick}>Clickadee Dee</button>
-  </section>
-) 
+  return (
+    <section>
+      <form onSubmit={handleFormSubmission}>
+        <h2>Just practicin'</h2>
+        <input onChange={handleFormInput}/>
+      </form>
+      <button onClick={handleButtonClick}>Click me click</button>
+    </section>
+  )
 }
-
-
-
-
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
